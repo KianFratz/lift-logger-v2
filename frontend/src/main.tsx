24ca40f22e0121766481400db5e6e6b3bff21af5
KianFratz <fratzkian@gmail.com>
2025-11-17 11:05:00 +0800

    Added 404 not found page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/Login.tsx'
+import { EmptyInputGroup } from './components/EmptyInputGroup.tsx'
 
 const router = createBrowserRouter([
   { path: "/", element: <App />},
-  { path: "/login", element: <Login /> }
+  { path: "/login", element: <Login /> },
+  { path: "*", element: <EmptyInputGroup />}
 ])
 
 createRoot(document.getElementById('root')!).render(
